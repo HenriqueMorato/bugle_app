a01c0868ea090b24ad29a07fbe7a324a7d03fae2
Henrique Morato <riquemorato@gmail.com>
2022-01-27 15:43:24 -0300

    Documentation and README

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+admin = User.create!(email: 'admin@admin.com', name: 'Admin',
+                     password: '12345678',
+                     role: :admin)
+
+User.create!(email: 'user@user.com', name: 'User', password: '12345678')
+
+course = Course.create!(title: 'Health Science 101',
+               description: 'Et sit officiis eaque.')
+Course.create!(title: 'Engineering 101',
+               description: 'Voluptates atque eos voluptate.')
+
+content = Content.create!(name: 'A video', user: admin, course: course)
+content.file.attach(
+  io: File.open(Rails.root.join('spec/fixtures/files/dramatic_chipmunk.mp4')),
+  filename: 'dramatic_chipmunk.mp4',
+  content_type: 'video/mp4'
+)
+content = Content.create!(name: 'Another video', user: admin, course: course)
+content.file.attach(
+  io: File.open(Rails.root.join('spec/fixtures/files/dramatic_chipmunk.mp4')),
+  filename: 'dramatic_chipmunk.mp4',
+  content_type: 'video/mp4'
+)
